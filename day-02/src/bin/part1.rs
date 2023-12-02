e637d84af80ff82981ebf8c70f3b94567d520693
Ariel Leyva <aleyva1509@gmail.com>
2023-12-02 08:05:57 -0600

    add: day 02 setup

---
+++ day-02/src/bin/part1.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input, 12, 13, 14);
+    dbg!(output);
+} 
+
+fn part1(input: &str, r:u64, g:u64, b:u64) -> String {
+    "8".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let output = part1(input, 12, 13, 14);
+        assert_eq!(output, "8");
+    }
+}
